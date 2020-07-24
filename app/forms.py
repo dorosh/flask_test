afff2480cf2dd3c947bb66324f8f3bc77f71b1e2
Andriy Doroshenko <dorosshh@gmail.com>
2020-07-24 17:17:27 +0300

    delete app

--- app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-
-class LinkForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
